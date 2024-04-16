d7890a9291f67ff8b8da34a156d49f340f98de1e
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-04-16 01:38:52 -0700

    Fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Square Class"""
 
-class square():
+
+class Square:
+    """a Square class"""
 
     def __init__(self, *args, **kwargs):
+        """making a new instance of Square object"""
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -10,18 +14,19 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """returns width and height of square"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
